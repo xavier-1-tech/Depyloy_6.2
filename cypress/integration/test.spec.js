bb2506d76607d9cf896a938bf73b40a2d06d33bc
xavier-1-tech <xaviertech05@gmail.com>
2021-09-25 14:14:59 -0400

    first commit

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,6 +1,6 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://172.31.26.57:5000/example-1')
+        cy.visit('http://52.23.203.221:5000/example-1')
 
         cy.get('h1')
             .invoke('text')
